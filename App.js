b98c474f3c20f403a25d784da6023b15b988bbe5
Neha Nainsukha <neha.nainsukha@flairlabs.com>
2021-09-27 22:49:25 +0530

    sign in and sign up screen

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoginScreen/>
+    // <SignupScreen/>
   );
 }
 
